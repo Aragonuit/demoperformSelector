b821ac8e147786768b92b1add9172fea565da135
Hung Nguyen <hungnguyenuit90hp@gmail.com>
2016-01-19 22:00:33 +0700

    Initial Commit

---
+++ demoperformSelector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoperformSelector
+//
+//  Created by AragonUit on 1/19/16.
+//  Copyright © 2016 AragonUit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
