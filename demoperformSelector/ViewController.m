3e4307c30d383af5b30b987fe5ee43745dffd793
AragonUit <aragonuit@MacBook-Air.local>
2016-01-19 22:19:59 +0700

    first commit

--- demoperformSelector/ViewController.m
+++ demoperformSelector/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
@@ -18,10 +19,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)ClickBtnCrunchData:(id)sender {
+//    [self performSelectorInBackground:@selector(doCrunchData) withObject:nil];
+    // chay do crundata trong background nen main thread ko bi block=> tuong tac duoc voi giao dien
+    [self performSelectorOnMainThread:@selector(doCrunchData) withObject:nil waitUntilDone:false];
+    //chay tren main thread nen trong 2 giay nhung tuong tac tren giao dien ko the thuc hien duoc
+    [self performSelectorOnMainThread:@selector(processData:) withObject:@{@"apple": @"táo", @"lemon": @"chanh"} waitUntilDone: true];
 }
-
+-(void)doCrunchData
+{
+    [NSThread sleepForTimeInterval:2];
+}
+- (void) processData: (NSDictionary*) data {
+    for (NSString* key in [data allKeys]) {
+        NSLog(@"%@ - %@", key, [data valueForKey:key]);
+}
+}
+- (void) hideSlider {
+    self.slider.hidden = true;
+}
 @end
